241149f7adfb212819e29c3dc88a8bb24a81f0c7
TheAgaveFairy <93168751+TheAgaveFairy@users.noreply.github.com>
2024-03-26 19:14:57 -0500

    transpose actually fixed, added constructor for a new matrix where values are initialized to a random number in range (-1,1)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod matrix;
+use std::error::Error;
 use crate::matrix::Matrix;
 fn main() {
     let test0 = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
@@ -7,29 +8,58 @@ fn main() {
 
     println!("Creating a matrix with {} rows, {} cols, and the data {:?}.",2,3,test0);
     let test_matrix0 = Matrix::new(2, 3, test0).expect("matrix 0 error");
-    println!("{}\n", test_matrix0.to_string());
+    println!("{}\n", test_matrix0);
 
     println!("Creating a matrix with {} rows, {} cols, and the data {:?}.",3,2,test1);
     let test_matrix1 = Matrix::new(3, 2, test1).expect("matrix 1 error");
-    println!("{}\n", test_matrix1.to_string());
+    println!("{}\n", test_matrix1);
 
     println!("Calculating the dot product of these two matrices:");
     let result_matrix = test_matrix0.dot(&test_matrix1).expect("error with dot product");
-    println!("{}\n", result_matrix.to_string());
+    println!("{}\n", result_matrix);
 
     println!("Multiplying that result by the scalar 0.5:");
     let result_matrix = result_matrix.scale(0.5);
-    println!("{}\n", result_matrix.to_string());
+    println!("{}\n", result_matrix);
 
     println!("Showing the first row of that result:\n{:?}\n", result_matrix.get_row(0));
     println!("Showing the first col of that result:\n{:?}\n", result_matrix.get_col(0));
 
     let added_matrix = result_matrix.add_matrix(&result_matrix).expect("Adding didn't work.");
-    println!("Adding this matrix to itself:\n{}\n", added_matrix.to_string());
+    println!("Adding this matrix to itself:\n{}\n", added_matrix);
 
     let transposed_matrix = test_matrix0.transpose();
-    println!("Transposed version:\n{}\n", transposed_matrix.to_string());
+    println!("Transposed version:\n{}\n", transposed_matrix);
 
-    let added_scalar = transposed_matrix.add_scalar(-9.5);
-    println!("Subtracting scalar 9.5:\n{}\n", added_scalar.to_string());
+    let added_scalar = transposed_matrix.add_scalar(-1.5);
+    println!("Subtracting scalar 1.5:\n{}\n", added_scalar);
+
+    let messed_up_matrix = added_scalar.map(&|v| v * 2.0 / 3.0);
+    println!("Passing function:\n{}\n", messed_up_matrix);
+
+    let test2 = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+    let long_matrix = Matrix::new(2,4,test2).expect("");
+    println!("Long matrix:\n{}\n",long_matrix);
+    println!("Long matrix transposed:\n{}\n",long_matrix.transpose());
+
+    let rand_matrix = Matrix::new_from_rand(3,4);
+    println!("Random matrix:\n{}\n",rand_matrix);
+
+    example().expect("TODO: panic message");
+}
+fn example() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path("data/mnist_test.csv").expect("Error reading csv.");
+    let mut i = 0;
+    for result in rdr.into_records() {
+        if i == 5 {break}
+        let record = result?;
+        let actual: u8 = record[0].parse().expect("File error.");
+        let mut pixels: Vec<u32> = Vec::new();
+        for field in record.iter().skip(1) {
+            pixels.push(field.parse::<u32>()?);
+        }
+        let sum: u32 = pixels.into_iter().sum();println!("This is a {}. {} pixel sum.", actual, sum);
+        i += 1;
+    }
+    Ok(())
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,6 @@
-    #[derive(Debug)]
+use std::fmt;
+use rand::{thread_rng, Rng};
+#[derive(Debug)]
     pub(crate) struct Matrix {
         pub num_rows: usize,
         pub num_cols: usize,
@@ -18,36 +20,30 @@
                 data,
             })
         }
-        pub(crate) fn to_string(&self) -> String {
-            let mut display = "[".to_string();
-            let (num_rows, num_cols) = self.get_shape();
-
-            for row in 0..num_rows {
-                if row > 0 { display.push(' '); }
-                display.push('[');
-                for col in 0..num_cols {
-                    let val = self.get(row, col);
-                    let mut temp = format!("{:.2}", val);
-                    if col < num_cols - 1 { temp.push_str(", "); }
-                    display.push_str(temp.as_str());
-                }
-                display.push(']');
-                if row < num_rows - 1 { display.push('\n'); }
+        pub(crate) fn new_from_rand(num_rows: usize, num_cols: usize) -> Self {
+            let length = num_rows * num_cols;
+            let mut data: Vec<f64> = Vec::with_capacity(length);
+            let mut rng = thread_rng();
+            for _ in 0..length {
+                data.push(rng.gen::<f64>() * 2.0 - 1.0);
             }
-            display.push(']');
-            display
+            Matrix{
+                num_rows,
+                num_cols,
+                data
+            }
         }
         fn calc_index(&self, row: usize, col: usize) -> usize {
             row * self.num_cols + col
         }
-        fn calc_new_index(row_wanted: usize, col_wanted: usize, num_rows: usize, num_cols: usize) -> usize {
-            println!("calc new index. want {}, {} in a matrix of size {}, {}", row_wanted, col_wanted, num_rows, num_cols);
-            row_wanted * num_cols + col_wanted
-        }
+        // fn calc_new_index(row_wanted: usize, col_wanted: usize, num_rows: usize, num_cols: usize) -> usize {
+        //     println!("calc new index. want {}, {} in a matrix of size {}, {}", row_wanted, col_wanted, num_rows, num_cols);
+        //     col_wanted * num_rows + row_wanted
+        // }
         fn get_shape(&self) -> (usize, usize) {
             (self.num_rows, self.num_cols)
         }
-        fn get_size(&self) -> usize {
+        fn length(&self) -> usize {
             self.num_rows * self.num_cols
         }
         fn get(&self, row: usize, col: usize) -> &f64 {
@@ -56,19 +52,14 @@
         pub(crate) fn get_row(&self, row: usize) -> Vec<f64> {
             let start = self.calc_index(row, 0);
             let end = self.calc_index(row + 1, 0);
-            //println!("GOT ROW {}: {:?}", row, self.data[start..end].to_vec());
             self.data[start..end].to_vec()
         }
         pub(crate) fn get_col(&self, col: usize) -> Vec<f64> {
             let mut our_col: Vec<f64> = Vec::new();
             for i in 0..self.num_rows {
-                our_col.push(self.get(i, col).clone());
+                our_col.push(*self.get(i, col));
             }
-            //println!("GOT COL {}: {:?}", col, our_col);
             our_col
-        }
-        fn check_is_valid(&self) -> bool {
-            self.data.len() == self.get_size()
         }
         pub(crate) fn dot(&self, other: &Matrix) -> Result<Matrix, &'static str> {
             if self.num_cols != other.num_rows {
@@ -109,7 +100,7 @@
             }
         }
         pub(crate) fn add_scalar(&self, scalar: f64) -> Self {
-            let mut new_data = Vec::with_capacity(self.get_size());
+            let mut new_data = Vec::with_capacity(self.length());
             for v in &self.data {
                 new_data.push(v + scalar);
             }
@@ -123,8 +114,8 @@
             if self.get_shape() != other.get_shape() {
                 return Err("Incompatible shapes for addition.")
             }
-            let mut new_data: Vec<f64> = Vec::with_capacity(self.get_size());
-            for i in 0..self.get_size() {
+            let mut new_data: Vec<f64> = Vec::with_capacity(self.length());
+            for i in 0..self.length() {
                 let sum = self.data[i] + other.data[i];
                 new_data.push(sum);
             }
@@ -135,21 +126,15 @@
             })
         }
         pub(crate) fn transpose(&self) -> Self {
-            let mut new_data: Vec<f64> = Vec::with_capacity(self.get_size());
+            let mut new_data: Vec<f64> = Vec::with_capacity(self.length());
             let mut temp_i: usize = 0;
-            // for r in 0..self.num_rows {
-            //     for c in 0..self.num_cols {
-            //         temp_i = c * self.num_rows + r; // THIS DIDNT WORK
-            //         println!("transpose ti: {}", temp_i);
-            //         new_data.push(self.data[temp_i].clone());
-            //     }
-            // }
-            for i in 0..self.get_size() {
-                new_data.push(self.data[temp_i].clone());
-                if i % 2 == 0 {
-                    temp_i += self.num_cols;
-                } else {
-                    temp_i -= self.num_rows;
+            let new_rows = self.num_cols;
+            let new_cols = self.num_rows;
+            for r in 0..new_rows {
+                for c in 0..new_cols {
+                    temp_i = c * new_rows + r;
+                    //println!("transpose ti: {}", Self::calc_new_index(r,c,new_rows, new_cols));
+                    new_data.push(self.data[temp_i].clone());
                 }
             }
 
@@ -159,4 +144,37 @@
                 data: new_data,
             }
         }
+        pub(crate) fn map(&self, function: &dyn Fn(f64) -> f64) -> Self {
+            let mut new_data: Vec<f64> = self.data.clone().into_iter().map(
+                |val| function(val)).collect();
+
+            Matrix {
+                num_rows: self.num_cols,
+                num_cols: self.num_rows,
+                data: new_data,
+            }
+
+        }
     }
+
+    impl fmt::Display for Matrix {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let mut display = "[".to_string();
+            let (num_rows, num_cols) = self.get_shape();
+
+            for row in 0..num_rows {
+                if row > 0 { display.push(' '); }
+                display.push('[');
+                for col in 0..num_cols {
+                    let val = self.get(row, col);
+                    let mut temp = format!("{:.2}", val);
+                    if col < num_cols - 1 { temp.push_str(", "); }
+                    display.push_str(temp.as_str());
+                }
+                display.push(']');
+                if row < num_rows - 1 { display.push('\n'); }
+            }
+            display.push(']');
+            write!(f, "{}", display)
+        }
+    } // to_string()
